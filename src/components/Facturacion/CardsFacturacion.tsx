01c00c52bcc2e5d15a17ec730cff4a8ed6c9fc17
Yasel David <yasel2k21@gmail.com>
2025-10-17 10:16:18 -0400

    guadado de pequeños avances

--- src/components/Facturacion/CardsFacturacion.tsx
+++ src/components/Facturacion/CardsFacturacion.tsx
@@ -18,7 +18,26 @@ export default function CardsFacturacion({
         <div className="flex items-end justify-between mt-5">
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
-              Total ventas del mes
+              Total ventas del mes (espc)
+            </span>
+            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
+              RD{" "}
+              {new Intl.NumberFormat("es-DO", {
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+              }).format(valorTotal)}
+            </h4>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6 col-span-2">
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <BoxIconLine className="text-gray-800 size-6 dark:text-white/90" />
+        </div>
+        <div className="flex items-end justify-between mt-5">
+          <div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Total ventas del mes (real)
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
               RD{" "}
@@ -57,6 +76,21 @@ export default function CardsFacturacion({
           <div>
             <span className="text-sm text-gray-500 dark:text-gray-400">
               Facturas pendientes
+            </span>
+            <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
+              {agotados ?? 0}
+            </h4>
+          </div>
+        </div>
+      </div>
+      <div className="rounded-2xl border border-gray-200 bg-white p-5 dark:border-gray-800 dark:bg-white/[0.03] md:p-6">
+        <div className="flex items-center justify-center w-12 h-12 bg-gray-100 rounded-xl dark:bg-gray-800">
+          <BoxIconLine className="text-gray-800 size-6 dark:text-white/90" />
+        </div>
+        <div className="flex items-end justify-between mt-5">
+          <div>
+            <span className="text-sm text-gray-500 dark:text-gray-400">
+              Monto en transito
             </span>
             <h4 className="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">
               {agotados ?? 0}
